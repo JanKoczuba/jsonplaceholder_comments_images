358517d8ee1425274b6da83bc7ef467b2cad36d5
Jan Koczuba <jan.koczuba@it-solve.pl>
2023-06-09 11:32:41 +0200

    feature-image_gallery-added image gallery, default error widget,

--- lib/presentation/home/image_gallery/image_gallery_page.dart
+++ lib/presentation/home/image_gallery/image_gallery_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 
+import '../../resource/app_padding.dart';
+import 'widget/image_gallery.dart';
+
 @RoutePage()
 class ImageGalleryPage extends StatefulWidget {
   const ImageGalleryPage({Key? key}) : super(key: key);
@@ -13,8 +16,11 @@ class _ImageGalleryPageState extends State<ImageGalleryPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('ImageGalleryPage'),
+      body: SafeArea(
+        child: Padding(
+          padding: AppPadding.page,
+          child: ImageGallery(),
+        ),
       ),
     );
   }
---
+++ lib/presentation/home/image_gallery/widget/image_item.dart
@@ -0,0 +1,22 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ImageItem extends StatelessWidget {
+  const ImageItem({
+    required this.imageUrl,
+    this.loadingWidget,
+    Key? key,
+  }) : super(key: key);
+
+  final String imageUrl;
+  final Widget? loadingWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      errorWidget: (context, url, error) => const Icon(Icons.error),
+      placeholder: (context, url) => loadingWidget ?? const SizedBox(),
+    );
+  }
+}
