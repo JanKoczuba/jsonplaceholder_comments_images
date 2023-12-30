0ac1d99369e605e3820dfdb3376ad01930a52929
jankoczuba <jan.koczuba@it-solve.pl>
2023-12-30 11:39:52 +0100

    Add unit tests for internet connection use cases

---
+++ test/domain/config/use_case/on_internet_conection_changed_use_case_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:jsonplaceholder_comments_images/domain/config/service/config_service.dart';
+import 'package:jsonplaceholder_comments_images/domain/config/use_case/on_internet_conection_changed_use_case.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockConfigService extends Mock implements ConfigService {}
+
+void main() {
+  late MockConfigService mockConfigService;
+  late OnInternetConnectionChanged onInternetConnectionChanged;
+
+  setUp(() {
+    mockConfigService = MockConfigService();
+    onInternetConnectionChanged =
+        OnInternetConnectionChanged(mockConfigService);
+  });
+
+  group('OnInternetConnectionChanged', () {
+    test('execute returns the same stream as ConfigService.onStatusChange', () {
+      const stream = Stream<InternetConnectionStatus>.empty();
+      when(() => mockConfigService.onStatusChange).thenAnswer((_) => stream);
+
+      expect(onInternetConnectionChanged.execute(), equals(stream));
+    });
+  });
+}
